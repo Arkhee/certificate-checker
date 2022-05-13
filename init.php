2d9be42b9ba6072df14bc6dcedf0e1c29231aee6
Yannick Bétemps <yannick@alternetic.com>
2022-05-13 15:02:16 +0200

    Fix et sécurisation avec clef de sécurité

--- init.php
+++ init.php
@@ -1,4 +1,4 @@
 <?php
 const __ROOT_FOLDER__ = __DIR__;
-if(!file_exists(__DIR__."/settings.php")) die("No settings file in the config folder, please use settings.sample.php as a template");
-if(!file_exists(__DIR__."/urls.json")) die("No url list in the config folder, please use urls.sample.json as a template");
+if(!file_exists(__DIR__."/config/settings.php")) die("No settings file in the config folder, please use settings.sample.php as a template");
+if(!file_exists(__DIR__."/config/urls.json")) die("No url list in the config folder, please use urls.sample.json as a template");
